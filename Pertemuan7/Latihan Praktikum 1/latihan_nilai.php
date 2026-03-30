cea532a307a4eddfcd53c72b3f52243dec267958
DiegoAndreasSimanjuntak <diegosimanjuntak2006@gmail.com>
2026-03-30 20:00:36 +0700

    Pertemuan 7 Praktikum 6

---
+++ Pertemuan7/Latihan Praktikum 1/latihan_nilai.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Input Nilai Mahasiswa</title>
+</head>
+<body>
+
+<h2>Form Nilai Mahasiswa</h2>
+
+<form action="proses.php" method="post">
+    Nama: <input type="text" name="nama"><br>
+    Nilai: <input type="number" name="nilai"><br>
+    <input type="submit" value="Proses">
+</form>
+
+</body>
+</html>
---
+++ Pertemuan7/Latihan Praktikum 2/proses.php
@@ -0,0 +1,31 @@
+<?php
+if (!empty($_POST['npm']) && !empty($_POST['nama'])) {
+    $npm = $_POST['npm'];
+    $nama = $_POST['nama'];
+    $prodi = $_POST['prodi'];
+    $semester = $_POST['semester'];
+    $ukt = $_POST['ukt'];
+
+    $diskon = 0;
+
+    if ($ukt >= 5000000) {
+        $diskon = 10;
+        if ($semester > 8) {
+            $diskon = 15;
+        }
+    }
+
+    $potongan = ($diskon / 100) * $ukt;
+    $bayar = $ukt - $potongan;
+
+    echo "NPM : $npm <br>";
+    echo "NAMA : $nama <br>";
+    echo "PRODI : $prodi <br>";
+    echo "SEMESTER : $semester <br>";
+    echo "BIAYA UKT : Rp. " . number_format($ukt,0,',','.') . ",-<br>";
+    echo "DISKON : $diskon% <br>";
+    echo "YANG HARUS DIBAYAR : Rp. " . number_format($bayar,0,',','.') . ",-<br>";
+} else {
+    echo "Data belum dikirim!";
+}
+?>
