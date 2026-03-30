277398829afd67c0787949bea5d6ab9f7cfb13a1
DiegoAndreasSimanjuntak <diegosimanjuntak2006@gmail.com>
2026-03-30 20:16:48 +0700

    Pertemuan 7 Praktikum 6

---
+++ Pertemuan7/Latihan Praktikum 2/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Pembayaran UKT</title>
+</head>
+<body>
+
+<h2>Form Pembayaran UKT</h2>
+<form action="proses.php" method="post">
+    NPM : <input type="text" name="npm"><br>
+    Nama : <input type="text" name="nama"><br>
+    Prodi : <input type="text" name="prodi"><br>
+    Semester : <input type="number" name="semester"><br>
+    Biaya UKT : <input type="number" name="ukt"><br>
+    <input type="submit" value="Hitung">
+</form>
+
+</body>
+</html>
